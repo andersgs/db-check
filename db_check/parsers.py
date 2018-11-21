f5e16b10ae1844e858fd2171953903bb767aee49
Anders Goncalves da Silva <andersgs@gmail.com>
2018-11-20 16:47:15 -0800

    Cleaned-up parsers

--- db_check/parsers.py
+++ db_check/parsers.py
@@ -8,10 +8,7 @@ import collections
 import pathlib
 import re
 
-import click
 import pandas as pd
-import sh
-from tabulate import tabulate
 
 
 def parse_cluster_id(line):
@@ -28,7 +25,7 @@ def parse_cluster_id(line):
 
 def parse_cluster_member(line, cluster_id):
     '''
-    If the line is a record in a cluster, parse the record
+    If the line is a record in a cluster, parse the record.
 
     Examples:
     0       1269nt, >71|z4,z32... *
@@ -40,14 +37,14 @@ def parse_cluster_member(line, cluster_id):
      seqid: 71|z4,z32,
      is_centroid: True,
      match: 1,
-     antigen: z4,z32}
+     cluster_id: 597}
 
      For 1
     {length: 1269,
      seqid: 71|z4,z32,
      is_centroid: False,
      match: 1,
-     antigen: z4,z32}
+     cluster_id: 597}
     '''
     # pattern is used to pull the percent identity
     pat = re.compile(r'[0-9]{1,3}\.?[0-9]?[0-9]?')
@@ -56,10 +53,8 @@ def parse_cluster_member(line, cluster_id):
     *_, length, seqid = line.strip().split(None, 2)
     length = length.replace("nt,", "")
     fasta_header, match = seqid.split("...")
-    *_, category = fasta_header.split("|")
     record['seqid'] = fasta_header[1:]
     record['length'] = length
-    record['category'] = category
     if match[-1] == '*':
         is_centroid = True
         match = 1.0
@@ -104,79 +99,34 @@ def parse_clustering(filename):
     return result
 
 
-def cluster_db(filename, threads=4):
+def parse_categories(dataframe, **parse_args):
     '''
-    Given a FASTA DB, cluster it using CD-HIT. In this case, using cd-hit-est.
+    A function to parse out categories from sequence IDs
+
+    parse_arguments can be:
+    - delimiter: character vector to split ID
+    - field: field number in 0-index designating which field to keep
+    OR
+    - regex: a regex expression with a single capture group
+    OR
+    - callback: a function that takes the seqid and returns the 
+        category
     '''
-    cdhit = sh.Command('cd-hit-est')
-    p = cdhit('-i', filename, '-o', 'cdhit', '-c',
-              '1.00', '-g', '1', '-T', threads, '-d', '0')
-    return "cdhit.clstr"
-
-
-def summarise_clustering(dataframe):
-    '''
-    Generate various summaries
-    '''
-    total_entries = dataframe.shape[0]
-    total_clusters = dataframe.clusterid.nunique()
-    n_unique_sequences = dataframe.seqid.nunique()
-    unique_categories = dataframe.groupby(
-        'clusterid')['category'].nunique().describe().to_frame().transpose()
-    cluster_size_dist = dataframe.groupby(
-        'clusterid')['clusterid'].count().describe().to_frame().transpose()
-    print("#"*80)
-    print("SOME SUMMARY INFORMATION...")
-    print()
-    print("Total records: {}".format(total_entries))
-    print("Total clusters: {}".format(total_clusters))
-    print("Total unique sequence IDs: {}".format(n_unique_sequences))
-    print()
-    print("#"*80)
-    print("SOME DETAILS...")
-    print()
-    print("Distribution of cluster sizes:")
-    print(cluster_size_dist)
-    print()
-    print("Summary of count of categories associated with each clusters:")
-    print(unique_categories)
-    print()
-    if total_entries > n_unique_sequences:
-        print("#"*80)
-        print("CHECKING DUPLICATE IDS...")
-        print()
-        ix = dataframe.duplicated('seqid', keep=False)
-        g = dataframe[ix].groupby("seqid")
-        for h in g.groups:
-            print("## Duplicate ID {}".format(h))
-            print(g.get_group(h))
-            print()
-    g = dataframe.groupby('clusterid')
-    ix = g.size() > 1
-    ix = sorted(ix.index[ix], key=int)
-    if len(ix) > 0:
-        print("#"*80)
-        print("LOOKING CLOSER...")
-        print()
-        print("Found clusters with more than one sequences...")
-        print("Checking if there are any with multiple categories...")
-        for i in ix:
-            h = g.get_group(i)
-            if h.category.nunique() > 1:
-                print("## Cluster {}".format(i))
-                print(h)
-                print()
-    print("#"*80)
-    print("Done!")
-
-
-@click.command()
-@click.argument("fasta")
-def run_db_check(fasta):
-    clusters = cluster_db(fasta)
-    tab = parse_clustering(clusters)
-    summarise_clustering(tab)
-
-
-if __name__ == "__main__":
-    run_db_check()
+    if "delimiter" in parse_args.keys():
+        # must check that delimiter and field are both set
+        # in the command line option
+        delimiter = parse_args['delimiter']
+        field = int(parse_args['field'])
+        dataframe['category'] = dataframe.seqid.str.split(
+            delimiter).str.get(field)
+        print(dataframe)
+    elif "regex" in parse_args.keys():
+        regex = parse_args['regex']
+        dataframe['category'] = dataframe.seqid.str.extract(
+            regex, expand=False)
+    elif "callback" in parse_args.keys():
+        callback = parse_args['callback']
+        dataframe['category'] = dataframe.seqid.apply(lambda s: callback(s))
+    else:
+        raise Exception("Not sure how to parse categories.")
+    return dataframe
